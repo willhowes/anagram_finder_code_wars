7eed81903b6338a144a89053acae0e4bbb942e8c
Will Howes <williameldenhowes@gmail.com>
2019-09-01 20:32:32 +0100

    Refactored

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,12 +1,8 @@
 def anagram_finder(word, words)
   word = word.split('').sort.join
-  p "WORD"
-  p word
   anagrams = []
   words.each_with_index do |word_to_check, index|
     word_to_check = word_to_check.split('').sort.join
-    p "WORD_TO_CHECK"
-    p word_to_check
     if word_to_check == word
       anagrams << words[index]
     end
