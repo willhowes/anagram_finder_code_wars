4ab9a822d09fe2505470b2e51b3b7160d74509f0
Will Howes <williameldenhowes@gmail.com>
2019-09-01 20:29:37 +0100

    Third test checking for words with more than letter passes

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,3 +1,15 @@
 def anagram_finder(word, words)
-  words[0].split
+  word = word.split('').sort.join
+  p "WORD"
+  p word
+  anagrams = []
+  words.each do |word_to_check|
+    word_to_check = word_to_check.split('').sort.join
+    p "WORD_TO_CHECK"
+    p word_to_check
+    if word_to_check == word
+      anagrams << word_to_check
+    end
+  end
+  anagrams
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,4 +8,8 @@ describe '.anagram_finder' do
   it 'finds a matching anagram with two in array' do
     expect(anagram_finder('a', ['a', 'b'])).to eq(['a'])
   end
+
+  it 'finds words with more than one letter which are anagrams' do
+    expect(anagram_finder('abba', ['aacd', 'aabb'])).to eq(['aabb'])
+  end
 end
